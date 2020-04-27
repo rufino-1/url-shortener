557697e375e1b79db74c3a04d399e45a91013d3b
rufinooregon <57548402+rufinooregon@users.noreply.github.com>
2020-04-27 05:49:44 -0500

    created your_url.html and modified app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # export FLASK_ENV=development
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html", name="Rufino")
+    return render_template("home.html")
 
-@app.route('/about')
-def about():
-    return "this is a url shortener!!!**"
+@app.route('/your-url')
+def your_url():
+    return render_template("your_url.html", code=request.args['code'])
 
